70d6fdf9d63a910c253c0bad50c016e505406a7e
Juhan Klaassen <juhan04@tlu.ee>
2025-02-13 08:59:04 +0000

    Kodutöö.

--- 01_funktsioon/nimeKontroll.ts
+++ 01_funktsioon/nimeKontroll.ts
@@ -1,14 +1,18 @@
+//Omaloodud funktsioon
+
+//Igaüks koostab temaatilise spetsiifilise funktsiooni ning demonstreerib selle tööd näitandmete abil.
+
+//Arvutus võib olla lihtne, aga lugejatel/kuulajatel võiks käsitletava teema kohta midagi õppida olla.
+
 function SõnaKontrollija(tähed: string, sõnad: string[]): string[] {
-    let sorteeriTähed = (s: string) => s.split("").sort().join(""); // Sorteerime tähed järjekorda
+    let sorteeriTähed = (s: string) => s.split("").sort().join("");
     let sisestatudTähed = sorteeriTähed(tähed);
     
     return sõnad.filter(sõna => sorteeriTähed(sõna) === sisestatudTähed);
 }
 
-// Näidisandmed: võimalike sõnade loend
 let sõnadeLoend = ["kass","koer","kala","küpsis","kool","tallinn","õlu"];
 
-// Testime funktsiooni erinevate sisenditega
 console.log(SõnaKontrollija("ksipsü", sõnadeLoend));
 console.log(SõnaKontrollija("saõklus", sõnadeLoend));
 console.log(SõnaKontrollija("litnnal", sõnadeLoend));
