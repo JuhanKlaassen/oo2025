1f51b5286f0bff73a5984e725bbbe637ba687be6
Juhan Klaassen <juhan04@tlu.ee>
2025-02-20 13:44:57 +0000

    Graafiline tegevus

---
+++ 03_klassikomplekt/takisti1.ts
@@ -0,0 +1,15 @@
+class Resistor{
+    protected r: number;
+    protected g;
+    constructor(r: number, g:object){
+        this.r=r; 
+        this.g=g;
+        this.draw();
+    }
+    draw(){
+        this.g.beginPath();
+        this.g.rect(50, 10, 100, 50);
+        this.g.stroke();
+        this.g.fillText(""+this.r, 80, 37);
+    }
+}
